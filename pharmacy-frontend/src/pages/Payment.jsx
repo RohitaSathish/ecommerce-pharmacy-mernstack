d232de630ddd1826a9b7d47d135667ec065764a7
Nikitha Rajkumar <nikitharajkumar18@gmail.com>
2025-12-16 09:55:54 +0530

    made payment page

---
+++ pharmacy-frontend/src/pages/Payment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Payment() {
+  return (
+    <div className="container">
+      <h2>Payment Page</h2>
+      <p>Complete your payment here.</p>
+      {/* Add payment form or integration here later */}
+    </div>
+  );
+}
+
+export default Payment;
